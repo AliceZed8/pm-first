f4e368d09c8fa4772bbbbea1a3c802f8fb9111b6
Alice_Zed <zokerfeed@gmail.com>
2022-11-08 23:58:42 +0200

    Изменения в проекте

--- Project SFML/inc/settings.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const float pi = 3.141592653589793238f;
-const unsigned int xCase = 80, yCase = 45;
-const unsigned int blockSize = 4;
-const unsigned int WIDTH = xCase*blockSize,
-                   HEIGHT = yCase*blockSize;
