6480743bcb58a4b270cee83a9f14f46e1be53651
AliceZed <zokerfeed@gmail.com>
2023-01-23 00:15:55 +0300

    Редактирование

--- Project/inc/settings.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PROJECT_SETTINGS_HPP
-#define PROJECT_SETTINGS_HPP
-
-const unsigned int WIDTH = 1280;
-const unsigned int HEIGHT = 720;
-const float PI = 3.14159265358979;
-const unsigned int blockSize = 12;
-#endif
