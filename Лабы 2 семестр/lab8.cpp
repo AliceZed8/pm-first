0b2c0999c2a00d67441401e3e2050dd74d5a8bef
AliceZed <zokerfeed@gmail.com>
2023-04-10 11:42:54 +0200

    edited

--- Лабы 2 семестр/lab8.cpp
+++ Лабы 2 семестр/lab8.cpp
@@ -2,34 +2,34 @@
 
 template <typename T>
 class Stack {
-private:
     struct Element {
         Element* last = nullptr;
         T data;
         Element() {}
-        Element(Element* elem, T dat) : last(elem), data(dat) {
+        Element(Element* elem, T dat): last(elem), data(dat) {
             std::cout << "Object\n" <<
-                "Last: " << last << "\n" <<
-                "Data: " << data << "\n" <<
-                "This: " << this << "\n\n";
+            "Last: " << last << "\n" << 
+            "Data: " << data << "\n" <<
+            "This: " << this << "\n\n";
         }
 
     };
+
     size_t sz, max_sz;
     Element* lst;
 
-public:
-    Stack(size_t max_size) : sz(0), lst(nullptr), max_sz(max_size) {}
+    public:
+    Stack(size_t max_size): sz(0), lst(nullptr), max_sz(max_size) {}
     ~Stack() {
-        std::cout << "Destructor\n";
         Element* el;
         while (lst != nullptr) {
             el = lst;
+            std::cout << "delete " << el << std::endl;
             lst = (*lst).last;
             delete el;
         }
     }
-
+    
     bool empty() {
         return (lst == nullptr) ? true : false;
     }
@@ -37,19 +37,19 @@ public:
     size_t size() {
         return sz;
     }
+
     void push(T data) {
-        if (sz == max_sz) throw std::overflow_error("Стек заполнен");
+        if (sz == max_sz) throw std::overflow_error("Stack is full");
         try {
             lst = new Element(lst, data);
             sz++;
-        }
-        catch (std::bad_alloc& e) {
+        } catch(std::bad_alloc &e) {
             throw;
         }
     }
 
-    T top() {
-        if (sz == 0) throw std::logic_error("Стек пуст");
+    T& top() {
+        if (sz == 0) throw std::logic_error("Stack is empty");
         return (*lst).data;
     }
 
@@ -66,20 +66,32 @@ public:
 };
 
 
-int main() {
+int main() { 
+    Stack<int> st(5);
+
+    printf("Size %d\n", st.size());
+    st.push(50);
+    printf("Size %d\n", st.size());
+    st.push(20);
+    printf("Size %d\n", st.size());
+    st.push(80);
+    printf("Size %d\n", st.size());
+
     
-        Stack<int> st(20);
+    for (int i = 0; i < 5; i++) {
+        try {
+            st.push(i);
+        } catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+            break;
+        }
+    }
 
-        printf("Size %d\n", st.size());
-        st.push(50);
-        printf("Size %d\n", st.size());
-        st.push(20);
-        printf("Size %d\n", st.size());
-        st.push(80);
-        printf("Size %d\n", st.size());
 
-        printf("Top %d\n", st.top());
-        st.pop();
-        printf("Top %d\n", st.top());
-
+    printf("Top %d\n", st.top());
+    st.top() = 2000;
+    printf("Top %d\n", st.top());
+    st.pop();
+    printf("Top %d\n", st.top());
+    st.pop();
 }
